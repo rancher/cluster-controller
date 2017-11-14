9564ac73ac8f6db1c4ec109bac4898e2b0608dfb
Alena Prokharchyk <alena@rancher.com>
2017-11-13 18:26:25 -0800

    Switched to using controllers from types repo

--- controller/clusterprovisioner/provisioner.go
+++ controller/clusterprovisioner/provisioner.go
@@ -1,20 +1,18 @@
 package provisioner
 
 import (
-	"fmt"
+	"context"
 
 	"reflect"
 
+	"github.com/rancher/cluster-controller/client"
 	"github.com/rancher/cluster-controller/controller"
-	"github.com/rancher/cluster-controller/controller/utils"
-	clusterV1 "github.com/rancher/types/apis/cluster.cattle.io/v1"
-	"github.com/sirupsen/logrus"
-	"k8s.io/client-go/tools/cache"
+	clusterv1 "github.com/rancher/types/apis/cluster.cattle.io/v1"
 )
 
 type Provisioner struct {
-	config    *controller.Config
-	syncQueue *utils.TaskQueue
+	clientSet         *client.V1
+	clusterController clusterv1.ClusterController
 }
 
 func init() {
@@ -22,105 +20,51 @@ func init() {
 	controller.RegisterController(p.GetName(), p)
 }
 
-func (p *Provisioner) Init(cfg *controller.Config) {
-	p.config = cfg
-	p.syncQueue = utils.NewTaskQueue("clusterprovisionersync", p.keyFunc, p.sync)
-
-	p.config.ClusterInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc:    p.handleClusterCreate,
-		DeleteFunc: p.handleClusterDelete,
-		UpdateFunc: p.handleClusterUpdate,
-	})
-}
-
-func (p *Provisioner) handleClusterCreate(obj interface{}) {
-	key, err := p.keyFunc(obj)
+func (p *Provisioner) Init(config string) error {
+	clientSet, err := client.NewClientSetV1(config)
 	if err != nil {
-		return
+		return err
 	}
-	logrus.Infof("Cluster created [%s]", key)
-	p.syncQueue.Enqueue(obj)
+	p.clientSet = clientSet
+	p.clusterController = p.clientSet.ClusterClientV1.Clusters("").Controller()
+	p.clusterController.AddHandler(p.sync)
+	return nil
 }
 
-func (p *Provisioner) handleClusterUpdate(old, current interface{}) {
-	key, err := p.keyFunc(current)
-	if err != nil {
-		return
-	}
-	if configChanged(old, current) {
-		logrus.Infof("Cluster [%s] updated with the new config", key)
-		p.syncQueue.Enqueue(current)
-	}
-}
-
-func configChanged(old, current interface{}) bool {
-	oldC := old.(*clusterV1.Cluster)
-	newC := current.(*clusterV1.Cluster)
+func configChanged(old *clusterv1.Cluster, current *clusterv1.Cluster) bool {
 	changed := false
-	if newC.Spec.AKSConfig != nil {
-		changed = !reflect.DeepEqual(newC.Spec.AKSConfig, oldC.Spec.AKSConfig)
-	} else if newC.Spec.GKEConfig != nil {
-		changed = !reflect.DeepEqual(newC.Spec.GKEConfig, oldC.Spec.GKEConfig)
-	} else if newC.Spec.AKSConfig != nil {
-		changed = !reflect.DeepEqual(newC.Spec.AKSConfig, oldC.Spec.AKSConfig)
+	if current.Spec.AKSConfig != nil {
+		changed = !reflect.DeepEqual(current.Spec.AKSConfig, old.Spec.AKSConfig)
+	} else if current.Spec.GKEConfig != nil {
+		changed = !reflect.DeepEqual(current.Spec.GKEConfig, old.Spec.GKEConfig)
+	} else if current.Spec.AKSConfig != nil {
+		changed = !reflect.DeepEqual(current.Spec.AKSConfig, old.Spec.AKSConfig)
 	}
 
 	return changed
 }
 
-func (p *Provisioner) handleClusterDelete(obj interface{}) {
-	key, err := p.keyFunc(obj)
-	if err != nil {
-		return
-	}
-	logrus.Infof("Cluster deleted: %s", key)
-	p.syncQueue.Enqueue(obj)
-}
-
-func (p *Provisioner) keyFunc(obj interface{}) (string, error) {
-	// Cluster object is not namespaced,
-	// but DeletionHandlingMetaNamespaceKeyFunc already handles it
-	return cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
-}
-
-func (p *Provisioner) sync(key string) error {
-	logrus.Infof("Syncing provisioning for cluster [%s]", key)
-	c, exists, err := p.config.ClusterInformer.GetStore().GetByKey(key)
-	if err != nil {
-		return fmt.Errorf("Failed to get cluster by name [%s] %v", key, err)
-	}
-	if !exists {
-		//TODO = handle removal using finalizer
-		err = p.deleteCluster(key)
-		if err != nil {
-			return fmt.Errorf("Failed to delete cluster [%s] %v", key, err)
-		}
+func (p *Provisioner) sync(key string, cluster *clusterv1.Cluster) error {
+	if cluster == nil {
+		// no longer exists if nil is passed to this call
+		return nil
 	} else {
-		cluster := c.(*clusterV1.Cluster)
-		err = p.createOrUpdateCluster(cluster)
-		if err != nil {
-			return fmt.Errorf("Failed to create/update cluster [%s] %v", key, err)
-		}
+		// TODO check delete annotation, and call delete method if present
+		// otherwise call create/update
+		return p.createOrUpdateCluster(cluster)
 	}
-
-	logrus.Infof("Successfully synced provisioning cluster [%s]", key)
-	return nil
 }
 
 func (p *Provisioner) deleteCluster(key string) error {
 	return nil
 }
 
-func (p *Provisioner) createOrUpdateCluster(cluster *clusterV1.Cluster) error {
-	//TODO call drivers to provision the cluster
+func (p *Provisioner) createOrUpdateCluster(cluster *clusterv1.Cluster) error {
 	return nil
 }
 
-func (p *Provisioner) Run(stopc <-chan struct{}) error {
-	go p.syncQueue.Run()
-
-	<-stopc
-	return nil
+func (p *Provisioner) Run(ctx context.Context) error {
+	return p.clusterController.Start(1, ctx)
 }
 
 func (p *Provisioner) GetName() string {
@@ -128,5 +72,4 @@ func (p *Provisioner) GetName() string {
 }
 
 func (p *Provisioner) Shutdown() {
-	p.syncQueue.Shutdown()
 }
--- controllers.go
+++ controllers.go
@@ -3,5 +3,4 @@ package main
 import (
 	// controllers
 	_ "github.com/rancher/cluster-controller/controller/clusterprovisioner"
-	_ "github.com/rancher/cluster-controller/controller/nodestats"
 )
