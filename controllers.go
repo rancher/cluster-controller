263da439c139364d5a892b3a4aafbe1b9abc5d31
kinarashah <kinarahs@usc.edu>
2017-11-21 17:13:20 -0800

    cluster heartbeat syncer
    On every syncInterval, this controller will check if ping from cluster-agent has received and update cluster condition status accordingly.

--- controllers.go
+++ controllers.go
@@ -2,5 +2,6 @@ package main
 
 import (
 	// controllers
+	_ "github.com/rancher/cluster-controller/controller/clusterheartbeat"
 	_ "github.com/rancher/cluster-controller/controller/clusterprovisioner"
 )
