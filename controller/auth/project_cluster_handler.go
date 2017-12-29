f1ada6577401a98aa5f6d8629838f36b43b0b3d4
Craig Jellick <craig@rancher.com>
2017-12-28 17:52:02 -0700

    Add creator as project-owner

--- controller/auth/project_cluster_handler.go
+++ controller/auth/project_cluster_handler.go
@@ -6,7 +6,9 @@ import (
 	corev1 "github.com/rancher/types/apis/core/v1"
 	"github.com/rancher/types/apis/management.cattle.io/v3"
 	"github.com/rancher/types/config"
+	"github.com/sirupsen/logrus"
 	v12 "k8s.io/api/core/v1"
+	v13 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -14,8 +16,10 @@ import (
 
 func newPandCLifecycles(management *config.ManagementContext) (*projectLifecycle, *clusterLifecycle) {
 	m := &mgr{
-		mgmt:     management,
-		nsLister: management.Core.Namespaces("").Controller().Lister(),
+		mgmt:       management,
+		nsLister:   management.Core.Namespaces("").Controller().Lister(),
+		prtbLister: management.Management.ProjectRoleTemplateBindings("").Controller().Lister(),
+		crtbLister: management.Management.ClusterRoleTemplateBindings("").Controller().Lister(),
 	}
 	p := &projectLifecycle{
 		mgr: m,
@@ -36,7 +40,7 @@ func (l *projectLifecycle) Create(obj *v3.Project) (*v3.Project, error) {
 		return obj, err
 	}
 
-	err = l.mgr.reconcileCreatorRTB(obj)
+	_, err = l.mgr.reconcileCreatorRTB(obj)
 	return obj, err
 }
 
@@ -62,7 +66,7 @@ func (l *clusterLifecycle) Create(obj *v3.Cluster) (*v3.Cluster, error) {
 		return obj, err
 	}
 
-	err = l.mgr.reconcileCreatorRTB(obj)
+	_, err = l.mgr.reconcileCreatorRTB(obj)
 	return obj, err
 }
 
@@ -76,12 +80,69 @@ func (l *clusterLifecycle) Remove(obj *v3.Cluster) (*v3.Cluster, error) {
 }
 
 type mgr struct {
-	mgmt     *config.ManagementContext
-	nsLister corev1.NamespaceLister
+	mgmt       *config.ManagementContext
+	nsLister   corev1.NamespaceLister
+	prtbLister v3.ProjectRoleTemplateBindingLister
+	crtbLister v3.ClusterRoleTemplateBindingLister
 }
 
-func (m *mgr) reconcileCreatorRTB(obj runtime.Object) error {
-	return nil
+func (m *mgr) reconcileCreatorRTB(obj runtime.Object) (runtime.Object, error) {
+	return v3.CreatorMadeOwner.Do(obj, func() (runtime.Object, error) {
+		metaAccessor, err := meta.Accessor(obj)
+		if err != nil {
+			return obj, err
+		}
+
+		typeAccessor, err := meta.TypeAccessor(obj)
+		if err != nil {
+			return obj, err
+		}
+
+		creatorID, ok := metaAccessor.GetAnnotations()["field.cattle.io/creatorId"]
+		if !ok {
+			logrus.Warnf("%v %v has no creatorId annotation. Cannot add creator as owner", typeAccessor.GetKind(), metaAccessor.GetName())
+			return obj, nil
+		}
+
+		rtbName := "creator"
+		om := v1.ObjectMeta{
+			Name:      rtbName,
+			Namespace: metaAccessor.GetName(),
+		}
+		subject := v13.Subject{
+			Kind: "User",
+			Name: creatorID,
+		}
+
+		switch typeAccessor.GetKind() {
+		case v3.ProjectGroupVersionKind.Kind:
+			if rtb, _ := m.prtbLister.Get(metaAccessor.GetName(), rtbName); rtb != nil {
+				return obj, nil
+			}
+			if _, err := m.mgmt.Management.ProjectRoleTemplateBindings(metaAccessor.GetName()).Create(&v3.ProjectRoleTemplateBinding{
+				ObjectMeta:       om,
+				ProjectName:      metaAccessor.GetName(),
+				RoleTemplateName: "project-owner",
+				Subject:          subject,
+			}); err != nil {
+				return obj, err
+			}
+		case v3.ClusterGroupVersionKind.Kind:
+			if rtb, _ := m.crtbLister.Get(metaAccessor.GetName(), rtbName); rtb != nil {
+				return obj, nil
+			}
+			if _, err := m.mgmt.Management.ClusterRoleTemplateBindings(metaAccessor.GetName()).Create(&v3.ClusterRoleTemplateBinding{
+				ObjectMeta:       om,
+				ClusterName:      metaAccessor.GetName(),
+				RoleTemplateName: "cluster-owner",
+				Subject:          subject,
+			}); err != nil {
+				return obj, err
+			}
+		}
+
+		return obj, nil
+	})
 }
 
 func (m *mgr) reconcileResourceToNamespace(obj runtime.Object) (runtime.Object, error) {
