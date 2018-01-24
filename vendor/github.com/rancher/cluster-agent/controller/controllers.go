654d42e3cc3ed034196c33b0d106fa6884a94f29
Darren Shepherd <darren@rancher.com>
2018-01-23 18:20:52 -0700

    Update vendor

--- vendor/github.com/rancher/cluster-agent/controller/controllers.go
+++ vendor/github.com/rancher/cluster-agent/controller/controllers.go
@@ -8,7 +8,7 @@ import (
 	"github.com/rancher/cluster-agent/controller/healthsyncer"
 	"github.com/rancher/cluster-agent/controller/nodesyncer"
 	"github.com/rancher/cluster-agent/controller/secret"
-	"github.com/rancher/cluster-agent/controller/statsyncer"
+	helmController "github.com/rancher/helm-controller/controller"
 	"github.com/rancher/types/config"
 	workloadController "github.com/rancher/workload-controller/controller"
 )
@@ -17,9 +17,9 @@ func Register(ctx context.Context, cluster *config.ClusterContext) error {
 	nodesyncer.Register(cluster)
 	healthsyncer.Register(ctx, cluster)
 	authz.Register(cluster)
-	statsyncer.Register(cluster)
 	eventssyncer.Register(cluster)
 	secret.Register(cluster)
+	helmController.Register(cluster)
 
 	workloadContext := cluster.WorkloadContext()
 	return workloadController.Register(ctx, workloadContext)
